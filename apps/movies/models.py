11ade6868d5a94055c6e2bf07b2cf05f1bf82677
Junwoo Hwang <capollux10@gmail.com>
2018-11-27 23:27:04 +0900

    Add iamge upload using ProcessedImageField, django-imagekit

--- apps/movies/models.py
+++ apps/movies/models.py
@@ -1,11 +1,20 @@
 from django.db import models
 from django.urls import reverse
+from imagekit.models import ProcessedImageField # 'pip install pillow pilkit django-imagekit' required
+from imagekit.processors import ResizeToFill
 
+def movie_image_path(instance, filename):
+    return 'movies/{}/{}'.format('images', filename) # Can set storing path using instance object's data
 
 class Movie(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
-    image = models.ImageField(blank=True) # 'pip install pillow' required
+    image = ProcessedImageField(
+        upload_to=movie_image_path,
+        processors=[ResizeToFill(500, 500)],
+        format='PNG', # or JPEG, etc.
+        blank=True,
+    ) 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
