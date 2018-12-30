0eaec27fb88ffc28c492a85ed61e79a7eab95374
Junwoo Hwang <capollux10@gmail.com>
2018-12-30 14:08:48 +0900

    Set AWS S3 for media files

--- whatsupmovie/settings.py
+++ whatsupmovie/settings.py
@@ -153,3 +153,12 @@ LOGOUT_REDIRECT_URL = '/'
 # Naver API Keys
 X_NAVER_CLIENT_ID = get_secret('X_NAVER_CLIENT_ID')
 X_NAVER_CLIENT_SECRET = get_secret('X_NAVER_CLIENT_SECRET')
+
+
+# AWS S3 Upload
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+AWS_ACCESS_KEY_ID = get_secret('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = get_secret('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = 'whatsupmovie'
+AWS_S3_REGION_NAME = 'ap-northeast-2'
+AWS_DEFAULT_ACL = None
