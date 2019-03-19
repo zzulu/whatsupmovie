19da8c78e9e4018859fd112e7220b47ed47ec982
Junwoo Hwang <capollux10@gmail.com>
2019-03-20 02:03:18 +0900

    Add padding top, bottom to profile textarea

--- apps/accounts/forms.py
+++ apps/accounts/forms.py
@@ -7,7 +7,7 @@ from django.contrib.auth.forms import UserCreationForm, PasswordChangeForm
 class ProfileForm(forms.ModelForm):
     nickname = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control profile-input'}), required=False)
     about = forms.CharField(widget=forms.Textarea(attrs={'class':'noresize form-control profile-input', 'rows':4}), required=False)
-    image = forms.FileField(widget=forms.FileInput(attrs={'class':'custom-file-input profile-input'}), required=False)
+    image = forms.FileField(widget=forms.FileInput(attrs={'class':'custom-file-input profile-input has-content'}), required=False)
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
