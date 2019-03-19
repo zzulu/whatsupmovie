1e49e6921250f86447acdc95bc43f78ca5bd2946
Junwoo Hwang <capollux10@gmail.com>
2019-03-20 01:02:50 +0900

    Add default score

--- apps/movies/forms.py
+++ apps/movies/forms.py
@@ -15,8 +15,8 @@ class RatingForm(forms.ModelForm):
         (1.0,'1.0'),
         (0.5,'0.5'),
     )
-    score = forms.ChoiceField(choices=SCORES, widget=forms.RadioSelect())
-    content = forms.CharField(widget=forms.Textarea(attrs={'class': 'noresize form-control comment-textarea', 'placeholder':'write a comment...', 'rows':'1'}))
+    score = forms.ChoiceField(choices=SCORES, widget=forms.RadioSelect(), initial=(2.5, '2.5'))
+    content = forms.CharField(widget=forms.Textarea(attrs={'class': 'noresize form-control comment-textarea', 'placeholder':'리뷰를 작성하세요!', 'rows':'1'}))
 
     class Meta:
         model = Rating
